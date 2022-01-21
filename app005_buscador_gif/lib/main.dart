649f5bef4e9ec90e59d42b4e2c1dd4bcb6914042
carolinepsantos <annecarolinepsantos@gmail.com>
2022-01-21 11:40:24 -0300

    :rocket: Aplicativo 005 - Buscador de Gifs
    Quinto aplicativo desenvolvido no curso cujo objetivo era se familiarizar com consumo de api, tratamento das respostas da api, compartilhamento de itens e praticar demais conceitos aprendidos.

---
+++ app005_buscador_gif/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:app005_buscador_gif/ui/gif_page.dart';
+import 'package:app005_buscador_gif/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+      )
+    ));
+}
+
