0861df81d834b6fbfb9ca99aa876a5949ef4ff35
carolinepsantos <annecarolinepsantos@gmail.com>
2022-02-04 08:31:25 -0300

    :art: Construção layout home_screen (Parte 01)

--- bloc_favoritos_youtube/lib/screens/home_screen.dart
+++ bloc_favoritos_youtube/lib/screens/home_screen.dart
@@ -5,6 +5,30 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/yt_logo_rgb_dark.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.star)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.search)
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
